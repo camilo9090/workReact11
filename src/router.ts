8c24a5b007b5d3a82e619f5175517c7d0cf6f3ee
camilo9090 <camilou9090@gmail.com>
2025-02-19 15:45:16 -0500

    Agregar validación de productos con express-validator y actualizar dependencias de @types/express

--- src/router.ts
+++ src/router.ts
@@ -1,29 +1,41 @@
-import {Router} from 'express'
+import { Router } from 'express'
 import { createProduct } from './handlers/product'
+import { body } from 'express-validator'
 
 
-const router=Router()
+const router = Router()
 
 //Rauting
 
-router.get('/',(req,res)=>{
-    const auth=true
+router.get('/', (req, res) => {
+    const auth = true
     res.json('Desde Get')
 })
-router.post('/',createProduct)
+router.post('/',
 
-router.put('/',(req,res)=>{
-    const auth=true
+    //validacion
+    body('name')
+        .notEmpty().withMessage('El nombre del Producto no debe estar vacio'),
+
+    body('price').isNumeric().withMessage('El valor no es valido').notEmpty()
+        .withMessage('El precio del Producto no debe estar vacio')
+        .custom(value => value > 0).withMessage('Valor debe ser mayor a 0'),
+
+    createProduct
+)
+
+router.put('/', (req, res) => {
+    const auth = true
     res.json('Desde put')
 })
 
-router.patch('/',(req,res)=>{
-    const auth=true
+router.patch('/', (req, res) => {
+    const auth = true
     res.json('Desde Patch')
 })
 
-router.delete('/',(req,res)=>{
-    const auth=true
+router.delete('/', (req, res) => {
+    const auth = true
     res.json('Desde Delete')
 })
 
