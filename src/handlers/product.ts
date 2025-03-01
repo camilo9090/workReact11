6c540b8b519114ef0d38015772226e383b31b0fa
camilo9090 <camilou9090@gmail.com>
2025-02-28 20:20:43 -0500

    Agregar pruebas para la eliminación de productos y mejorar la gestión de errores en las rutas de productos

--- src/handlers/product.ts
+++ src/handlers/product.ts
@@ -5,51 +5,32 @@ import Product from '../models/Product.model';
 
 export const getProduct = async (req: Request, res: Response) => {
 
-    try {
-        const products = await Product.findAll({
-            order: [
-                ['id', 'DESC']
-            ],
-            attributes: { exclude: ['createdAt', 'updatedAt'] }
-        })
-        res.json({ data: products })
-    } catch (error) {
-        console.log(error);
-
-    }
+    const products = await Product.findAll({
+        order: [
+            ['id', 'DESC']
+        ],
+        attributes: { exclude: ['createdAt', 'updatedAt'] }
+    })
+    res.json({ data: products })
 }
 
 export const getProductById = async (req: Request, res: Response) => {
 
-    try {
-        console.log(req.params.id);
-        const { id } = req.params
-        const product = await Product.findByPk(id)
+    console.log(req.params.id);
+    const { id } = req.params
+    const product = await Product.findByPk(id)
 
-        if (!product) {
-            return res.status(404).json({ error: 'Producto no encontrado' })
-
-        }
-
-        res.json({ data: product })
-
-    } catch (error) {
-        console.log(error);
+    if (!product) {
+        return res.status(404).json({ error: 'Producto no encontrado' })
 
     }
+
+    res.json({ data: product })
 }
 
 export const createProduct = async (req: Request, res: Response) => {
-    try {
-
-        const product = await Product.create(req.body)
-        res.status(201).json({ data: product })
-    } catch (error) {
-
-        console.log(error);
-
-
-    }
+    const product = await Product.create(req.body)
+    res.status(201).json({ data: product })
 }
 
 export const updateProduct = async (req: Request, res: Response) => {
@@ -69,33 +50,33 @@ export const updateProduct = async (req: Request, res: Response) => {
 }
 
 export const updateAvailability = async (req: Request, res: Response) => {
-    const { id } = req.params
+    const { id } = req.params
     const product = await Product.findByPk(id)
 
-    if(!product) {
+    if (!product) {
         return res.status(404).json({
             error: 'Producto No Encontrado'
         })
     }
-    
+
     // Actualizar
     product.availability = !product.dataValues.availability
     await product.save()
-    res.json({data: product})
+    res.json({ data: product })
 }
 
 export const deleteProduct = async (req: Request, res: Response) => {
- 
-    const { id } = req.params
+
+    const { id } = req.params
     const product = await Product.findByPk(id)
 
-    if(!product) {
+    if (!product) {
         return res.status(404).json({
             error: 'Producto No Encontrado'
         })
     }
 
     await product.destroy()
-    res.json({data:'Producto Eliminado'})
+    res.json({ data: 'Producto Eliminado' })
 
 }
