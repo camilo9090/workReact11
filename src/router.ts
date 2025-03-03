ffddfc8ffd5c20703dc5c32546d27f0c1c44c2db
camilo9090 <camilou9090@gmail.com>
2025-03-02 19:25:09 -0500

    Agregar documentación Swagger para el esquema de productos y el endpoint de obtención de productos

--- src/router.ts
+++ src/router.ts
@@ -5,8 +5,56 @@ import { handlersInputErrors } from './middleware'
 
 
 const router = Router()
+/**
+* @swagger
+* components:
+*   schemas:
+*     Product:
+*       type: object
+*       properties:
+*         id:  
+*           type: integer
+*           description: El ID del producto
+*           example: 1
+*         name:
+*             type: string
+*             description: El nombre del producto
+*             example: Teclado Gamer
+* 
+*         price:
+*             type: number
+*             description: El precio del producto
+*             example: 500
+* 
+*         availability:
+*             type: boolean
+*             description: La disponibilidad del producto
+*             example: true
+
+ * 
+ * 
+ */
+
 
 //Rauting
+
+/**
+ * @swagger
+ * /api/products:
+ *       get:
+ *         summary: obtener todos los productos
+ *         tags: [Products]
+ *         description: Retorna todos los productos
+ *         responses:
+ *             200:
+ *                description: Successfull response
+ *                content:
+ *                   application/json:
+ *                     schema:
+ *                        type: array
+ *                        items:
+ *                          $ref: '#/components/schemas/Product'
+ */
 router.get('/', getProduct)
 router.get('/:id',
 
@@ -18,6 +66,7 @@ router.get('/:id',
 router.post('/',
 
     //validacion
+
     body('name')
         .notEmpty().withMessage('El nombre del Producto no debe estar vacio'),
 
